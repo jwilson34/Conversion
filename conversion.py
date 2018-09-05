f85f0723f331e1b8d4f350b062365f2da228f4a0
Jamie Wilson <jwilson34@qub.ac.uk>
2018-09-05 11:28:31 +0100

    This is a comment

--- conversion.py
+++ conversion.py
@@ -1,11 +1,12 @@
-def kelvinto_celsius(temp):
-	return temp - 273.15
+def kelvin_to_celsius(temp):
+        return temp - 273.15
 
 
-def celsius_to_fahrenheit(temp):
+def celsius_to_fahr(temp):
 	return temp*(9/5) + 32 
 
 def kelvin_to_fahr(temp):
 	temp_c= kelvin_to_celsius(temp)
 	result = celsius_to_fahrenheit(temp_c)
 	return result
+
